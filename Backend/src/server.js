df3e39d2cd2dd44d4c15fda71e1597122479c97c
Darshan Hangoje <darshangoje@gmail.com>
2025-11-06 15:34:36 +0530

    first commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import path from 'path';
+
 
 dotenv.config();
 
@@ -9,9 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+const _dirname = path.resolve();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
 
 
 app.listen(PORT, () => {
